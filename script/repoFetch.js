4d21d832c04ed57c8b07f1b38f3867735bfd953b
ZTechDev <137308015+ZTechDev06@users.noreply.github.com>
2024-07-27 16:26:04 +0200

    Update repoFetch.js

--- script/repoFetch.js
+++ script/repoFetch.js
@@ -1,2 +1,21 @@
-// listrepos("ziadOUA", document.getElementById("repo-list-container")).then(reposcount => {
-// });
+function fetchReadme() {
+    var user = 'ZTechDev06';
+    var repo = 'ZTechDev06';
+    var readmeURL = 'https://api.github.com/repos/' + user + '/' + repo + '/readme';
+    
+    // Display loading message
+    $(".info-wrapper").html('<p>In Making...</p>');
+    
+    $.get(readmeURL, function(data) {
+        var readmeContent = atob(data.content);
+        var readmeHTML = marked.parse(readmeContent);
+        $(".info-wrapper").html('<div class="repo-readme-wrapper"><h2><p align="center">' + repo + '</h2>' + readmeHTML + '</div>');
+    }).fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Error fetching README.md:", textStatus, errorThrown); // Log the error
+        $(".info-wrapper").html('<p>Failed to fetch README.md for ' + repo + '.</p>');
+    });
+}
+
+$(document).ready(function() {
+    fetchReadme();
+});
